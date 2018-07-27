a0e5f391b04678e6dd7bce9d31a6f2d69aef3cba
unknown <jobkung555@gmail.com>
2018-07-27 11:59:06 +0700

    Second commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import rootReducers from './reducers'
 const store = createStore(rootReducers)
-store.subscribe(()=>console.log("test"+store.getState()))
+store.subscribe(()=>console.log(store.getState()))
 ReactDOM.render(
     <Provider store={store}>
         <NepNep/>
